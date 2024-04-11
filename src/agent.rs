ee27722f54d89ef26232245924993ce1c6b32bca
Alex Day <alex@alexday.me>
2024-04-10 20:07:34 -0400

    do n rollouts before costing

--- src/agent.rs
+++ src/agent.rs
@@ -15,38 +15,49 @@ impl DiffDriveAgent {
         Self { state, goal }
     }
 
-    pub fn at_goal(&self, eps: f64) -> bool {
-        (self.state - self.goal).norm() <= eps
+    fn goal_dist(&self) -> f64 {
+        (self.state - self.goal).norm()
     }
 
-    fn query(&self, action: &Vector2<f64>, dt: f64) -> Vector3<f64> {
+    pub fn at_goal(&self, eps: f64) -> bool {
+        self.goal_dist() <= eps
+    }
+
+    fn query(&self, state: &Vector3<f64>, action: &Vector2<f64>, dt: f64) -> Vector3<f64> {
         Vector3::<f64>::new(
-            self.state[0] + action[0] * f64::cos(action[1]) * dt,
-            self.state[1] + action[0] * f64::sin(action[1]) * dt,
-            self.state[2] + action[1] * dt,
+            state[0] + action[0] * f64::cos(action[1]) * dt,
+            state[1] + action[0] * f64::sin(action[1]) * dt,
+            state[2] + action[1] * dt,
         )
     }
 
-    fn cost(&self, action: &Vector2<f64>, dt: f64) -> f64 {
-        (self.goal - self.query(action, dt)).norm()
+    fn cost(&self, action: &Vector2<f64>, rollout_iters: i32, dt: f64) -> f64 {
+        let mut rollout_state = self.state;
+        for _ in 0..rollout_iters {
+            rollout_state = self.query(&rollout_state, action, dt);
+        }
+
+        (self.goal - rollout_state).norm()
     }
 
-    pub fn get_next_best_action(&self, iters: i32, dt: f64) -> (Vector2<f64>, f64) {
+    pub fn get_next_best_action(&self, control_iters: i32, rollout_iters: i32, dt: f64) -> (Vector2<f64>, f64) {
         let mut curr_best_action = Vector2::<f64>::zeros();
         let mut curr_best_cost = f64::INFINITY;
         let mut rng = rand::thread_rng();
-        for _ in 0..iters {
+        for _ in 0..control_iters {
             let candidate_action = Vector2::<f64>::new(
                 rng.gen::<f64>() * (DiffDriveAgent::ACTION_UB[0] - DiffDriveAgent::ACTION_LB[0]) + DiffDriveAgent::ACTION_LB[0],
                 rng.gen::<f64>() * (DiffDriveAgent::ACTION_UB[1] - DiffDriveAgent::ACTION_LB[1]) + DiffDriveAgent::ACTION_LB[1],
             );
-            let candidate_cost = self.cost(&candidate_action, dt);
+            let candidate_cost = self.cost(&candidate_action, rollout_iters, dt);
 
             if candidate_cost < curr_best_cost {
                 curr_best_action = candidate_action;
                 curr_best_cost = candidate_cost;
             }
         }
+
+        println!("Best action: {} cost {}", curr_best_action, curr_best_cost);
 
         (curr_best_action, curr_best_cost)
     }
